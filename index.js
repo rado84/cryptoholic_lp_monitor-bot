b721f104b04f3828e733fc388ea9002619e815b1
vander moleker <vandermoleker@gmail.com>
2024-07-05 13:33:55 -0700

    add

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ bot.command("finish",ctx=>{
 })
 
 const connection = new web3.Connection(process.env.RPC_API);//Web3.js Connection
+const helius_connection=new web3.Connection(process.env.HELIUS_RPC);
 
 const signatures=[];
 const monitorProcesses={}
@@ -121,6 +122,109 @@ connection.onLogs(TOKEN_PROGRAM_ID,async ({logs,signature,err})=>{
     }
 })
 connection.onLogs(raydium_program_id,async ({logs,signature,err})=>{
+    // console.log(signature)
+    if(logs.some(log=>log.includes("initialize2"))){
+        try {
+            const tx=await connection.getParsedTransaction(signature,{maxSupportedTransactionVersion:0});
+            const accounts = (tx?.transaction.message.instructions).find(ix => ix.programId.toBase58() ===process.env.RAYDIUM_OPENBOOK_AMM).accounts;
+            
+            if (!accounts) {
+                console.log("No accounts found in the transaction.");
+                return;
+            }
+
+            const tokenAIndex = 8;
+            const tokenBIndex = 9;
+
+            const tokenAAccount = accounts[tokenAIndex];
+            const tokenBAccount = accounts[tokenBIndex];
+            const targetToken=(tokenAAccount.toBase58()==SOL_MINT_ADDRESS)?tokenBAccount.toBase58():tokenAAccount.toBase58();
+            const quoted=(tokenAAccount.toBase58()==SOL_MINT_ADDRESS)?true:false;
+            
+            newPoolMonitorProcesses[targetToken]=true;
+            newPoolMonitorProcesses[targetToken]=fork(newPoolMonitorProcessPath);
+            newPoolMonitorProcesses[targetToken].send({token:targetToken,quoted:quoted});
+            newPoolMonitorProcesses[targetToken].on("message",message=>{
+                botClients.forEach(async oneClient=>{
+                    // await bot.api.sendPhoto(oneClient,message.image);
+                    bot.api.sendMessage(oneClient,
+                        `<b>⚡️ New Pool ⚡️</b>\n\n<b>Name : </b>${message.name}\n<b>Description : </b>\n${message.description}\n<b>Symbol : </b>${message.symbol}\n\n<b>Liquidity : </b><b>${message.lpValue} SOL</b>\n<b>Market Cap : </b><b>${Number(message.marketCap).toFixed(2)} $</b>\n<b>Largest holdings : </b>\n<b>${message.largestHolders}</b> wallets are owning <b>${Number(message.largestHoldingsPercentage).toFixed(2)} %</b> \n\n<b>Mint : </b>\n\n<code>${targetToken}</code>\n\n<a href="https://solscan.io/token/${targetToken}">Solscan</a> | <a href="https://photon-sol.tinyastro.io/en/lp/${message.poolId}">Photon</a> | <a href="https://dexscreener.com/solana/${message.poolId}">DexScreener</a>\n`
+                        ,{
+                            parse_mode:"HTML",
+                            link_preview_options:{
+                                is_disabled:true
+                            },
+                        }
+                    );
+                })
+            })
+            newPoolMonitorProcesses[targetToken].on("exit",(code)=>{
+                delete newPoolMonitorProcesses[targetToken];
+            })
+
+        } catch (error) {
+            console.log(error)
+        }
+        
+    }
+})
+helius_connection.onLogs(TOKEN_PROGRAM_ID,async ({logs,signature,err})=>{
+    if(logs.some(oneLog=>oneLog.includes("burn")||oneLog.includes("Burn")||oneLog.includes("burnChecked"))){
+        if(signature=="1111111111111111111111111111111111111111111111111111111111111111") return;
+        // console.log(`From Polling : ${signature}`)
+        if(signatures.some(sig=>sig==signature)) return;
+        signatures.push(signature)
+        if(signatures.length>100) signatures.shift();
+        // console.log(signature)
+        try {
+            const tx=await connection.getParsedTransaction(signature,{maxSupportedTransactionVersion:0});
+            const instruction= tx?.transaction?.message?.instructions.find(ix => {
+                return ix?.parsed?.type=="burn"||ix?.parsed?.type=="Burn"||ix?.parsed?.type=="burnChecked"});
+            if(!instruction) return;
+            
+            const mint=instruction.parsed.info.mint;
+            const totalSupply=await connection.getTokenSupply(new web3.PublicKey(mint));
+            // console.log(totalSupply.value)
+            if(totalSupply.value.uiAmount>0) {
+                // console.log("NOT ALL BURNED TOKEN!!!");
+                // console.log({amount: totalSupply.value, token:`https://solscan.io/token/${mint}`})
+                return;
+            }
+            if(monitorProcesses[mint]) {
+                // console.log("ALREADY MONITORING!!!")
+                return;
+            }
+            monitorProcesses[mint]=true;
+            monitorProcesses[mint]=fork(monitorProcessPath);
+            monitorProcesses[mint].send({token:mint})
+            monitorProcesses[mint].on("message",async message=>{
+                if(message.marketCap){
+                    botClients.forEach(async oneClient=>{
+                        // await bot.api.sendPhoto(oneClient,message.image);
+                        bot.api.sendMessage(oneClient,
+                            `<b>🔥 Burned Pool 🔥</b>\n\n<b>Name : </b><b>${message.name}</b>\n<b>Description : </b>\n<b>${message.description}</b>\n<b>Symbol : </b><b>${message.symbol}</b>\n\n<b>Liquidity : </b><b>${message.lpValue} SOL</b>\n<b>Market Cap : </b><b>${Number(message.marketCap).toFixed(2)} $</b>\n<b>Largest holdings : </b>\n<b>${message.largestHolders}</b> wallets are owning <b>${message.largestHoldingsPercentage} %</b> \n\n<b>Mint : </b>\n\n<code>${message.token}</code>\n\n<a href="https://solscan.io/token/${mint}">Solscan</a> | <a href="https://photon-sol.tinyastro.io/en/lp/${message.poolId}">Photon</a> | <a href="https://dexscreener.com/solana/${message.poolId}">DexScreener</a>\n`
+                            ,{
+                                parse_mode:"HTML",
+                                link_preview_options:{
+                                    is_disabled:true
+                                },
+                            }
+                        );
+                        
+                    })
+                    
+                }
+            })
+            monitorProcesses[mint].on("exit",(code)=>{
+                delete monitorProcesses[mint];
+            })
+        } catch (error) {
+            console.log(error)
+        }
+        
+    }
+})
+helius_connection.onLogs(raydium_program_id,async ({logs,signature,err})=>{
     // console.log(signature)
     if(logs.some(log=>log.includes("initialize2"))){
         try {
