1d2ceb4a4a93099280c3b17a3b26c22722668ff9
vander moleker <vandermoleker@gmail.com>
2024-07-11 13:21:22 -0700

    update

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const raydium_program_id=new web3.PublicKey(process.env.RAYDIUM_OPENBOOK_AMM);
 
 const PUMPFUN_MARKET_CAP=80;//SOL
 const NUMBER_OF_BUY_TRADES=40//Number
-const NUMBER_OF_TRADES=45;
+const NUMBER_OF_TRADES=30;
 
 
 
