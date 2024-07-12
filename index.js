ea9f657c78584e46c2d414ec89d778d4264eb59b
vander moleker <vandermoleker@gmail.com>
2024-07-11 17:43:28 -0700

    test

--- index.js
+++ index.js
@@ -225,7 +225,7 @@ client.getVersion()
         blocksMeta: {},
         accountsDataSlice: [],
         ping: undefined,
-        commitment: Client.CommitmentLevel.CONFIRMED
+        commitment: Client.CommitmentLevel.PROCESSED
     })
 
     // Sending a subscription request.
