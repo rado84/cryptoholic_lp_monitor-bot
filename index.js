b972bec3f6cc38c92b3a69a13354d3c718404906
vander moleker <vandermoleker@gmail.com>
2024-07-12 06:32:54 -0700

    update

--- index.js
+++ index.js
@@ -265,7 +265,7 @@ client.getVersion()
                 vote: false,
                 failed: false,
                 signature: undefined,
-                accountInclude: ["675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8","6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P"],
+                accountInclude: ["675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8"],
                 accountExclude: [],
                 accountRequired: [],
             },
